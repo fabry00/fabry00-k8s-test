d0279670ce37f1c7794d670fa5ec6ee28ab19ce1
Fabrizio Faustinoni <fabry_00@hotmail.com>
2020-03-22 14:54:44 +0100

    test

---
+++ services/api-gateway-service/src/main/java/com/gateway/example/info/InfoService.java
@@ -0,0 +1,73 @@
+package com.gateway.example.info;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gateway.example.config.ApiGatewayProperties;
+import com.google.common.collect.ImmutableSet;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.fluent.Request;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class InfoService {
+
+    private HttpClient httpClient;
+    private ApiGatewayProperties properties;
+
+    Set<Info.ServiceHealth> getHealth() {
+        return ImmutableSet.copyOf(properties
+                .getServicesList()
+                .entrySet()
+                .parallelStream()
+                .map(this::fetchServiceHealth)
+                .collect(Collectors.toSet()));
+
+    }
+
+    private Info.ServiceHealth fetchServiceHealth(Map.Entry<String, String> service) {
+        final String url = String.format("%s/actuator/health", service.getValue());
+        log.info("Querying: {} at {}", url, service.getKey());
+        try {
+            String response = Request.Get(url)
+                    .execute()
+                    .returnContent().asString();
+            return new Info.ServiceHealth(service.getKey(), responseToHealth(response));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new Info.ServiceHealth(service.getKey(), Status.UNKNOWN.toString());
+    }
+
+    private String responseToHealth(String response) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            HealthResponse health = mapper.readValue(response, HealthResponse.class);
+            if (health.status.equals(Status.UP.toString())) {
+                return Status.UP.toString();
+            }
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return Status.DOWN.toString();
+
+    }
+
+    @Data
+    @NoArgsConstructor
+    private static class HealthResponse {
+        private String status;
+    }
+}
